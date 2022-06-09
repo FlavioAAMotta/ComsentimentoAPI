f8144f13ff0bfdd4f12ae2b7d86e74945018b3b0
FlavioAAMotta <flavio.motta@sga.pucminas.br>
2022-06-09 14:33:17 -0300

    FEATURE: Receive PDF file

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import {AddressInfo} from "net";
 import cors from "cors";
 import { noticeRouter } from "./routes/NoticeRouter";
 import { userRouter } from "./routes/UserRouter";
+import multer from "multer";
+const FormData = require('form-data');
+const upload = multer({ dest: './uploads/' })
 
 const app = express();
 
@@ -10,6 +13,13 @@ app.use(express.json());
 app.use(cors())
 app.use("/users", userRouter);
 app.use("/notices", noticeRouter);
+app.use('/pdf', express.static(__dirname + '/uploads'));
+// Single file
+app.post("/pdfFile", upload.single("file"), (req, res) => {
+  console.log(req.body)
+  console.log(req.file)
+  return res.send("Single file")
+})
 
 const server = app.listen(3003, () => {
   if (server) {
